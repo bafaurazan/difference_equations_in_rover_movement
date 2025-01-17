2644d0eae05f41370788d95daee96266404a3ce8
Rafal Bazan <rafalbe777@gmail.com>
2025-01-17 03:20:11 +0100

    adding rover movement example in readme

--- ros2_ws/README.md
+++ ros2_ws/README.md
@@ -7,9 +7,83 @@ This project is a ROS 2 package designed to control a rover using Python-based p
 - CLI tools for quick testing and debugging.
 - A Python publisher for automated control of wheel movements.
 
-## Dependencies
+## Installation
 
-- ROS2 humble
+### Install ROS2 Humble
+Follow the official [ROS2 Humble installation guide](https://docs.ros.org/en/humble/Installation.html).
+
+### Setup the Workspace
+1. Clone this repository:
+```bash
+    cd /difference_equations_in_rover_movement/TrailblazerML
+```
+2. Install dependencies:
+    ```bash
+    sudo apt update && sudo apt install -y \
+      python3-colcon-common-extensions \
+      ros-humble-gazebo-ros-pkgs \
+      ros-humble-ros2-control \
+      ros-humble-ros2-controllers \
+      ros-humble-gazebo-ros2-control \
+      ros-humble-position-controllers \
+      ros-humble-xacro \
+      joystick \
+      jstest-gtk \
+      evtest \
+      ros-humble-twist-mux \
+      ros-humble-rviz2
+    ```
+
+3. Build the workspace:
+    ```bash
+    colcon build --symlink-install
+    source install/setup.bash
+    ```
+
+---
+
+## Running the Project
+1. install meshes
+```
+chmod +x scripts/dowload_rover_stl.sh 
+scripts/dowload_rover_stl.sh 
+```
+## Add meshes to Gazebo
+1. Go to gazebo folder:
+```bash
+   mkdir ~/.gazebo/models
+   cd ~/.gazebo/models
+   mkdir gazebo_viz
+```
+2. Add meshes:
+```bash
+  cp -r <Path_to_project>/TrailblazerML/src/gazebo_viz/meshes ./gazebo_viz
+```
+
+### Launching Simulation
+1. test simulation
+```bash
+    cd ~/difference_equations_in_rover_movement/TrailblazerML
+    source install/local_setup.bash
+```
+1. Launch the Gazebo simulator with the robot:
+```bash
+    ros2 launch gazebo_viz launch_sim.launch.py
+```
+2. To visualize the robot in RViz:
+```bash
+    ros2 launch gazebo_viz rsp.launch.py
+```
+
+### Teleoperation
+To control the robot manually:
+1. Connect your joystick or set up keyboard teleoperation.
+2. Launch the teleoperation node:
+```bash
+    ros2 launch rover_teleop_twist_joy teleop_twist_launch.py
+```
+   
+---
 
 ## 1. After cloning repository or changing publisher:
 
@@ -52,4 +126,19 @@ ros2 topic pub --once /diff_drive_controller_right/cmd_vel_unstamped geometry_ms
 ros2 topic pub --once /diff_drive_controller_left/cmd_vel_unstamped geometry_msgs/Twist "{linear: {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"
 
 ros2 topic pub --once /diff_drive_controller_right/cmd_vel_unstamped geometry_msgs/Twist "{linear: {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"
+```
+
+# Fast test
+```bash
+cd ~/difference_equations_in_rover_movement/TrailblazerML
+source install/local_setup.bash
+ros2 launch gazebo_viz launch_sim.launch.py
+```
+---
+```bash
+cd ~/difference_equations_in_rover_movement/ros2_ws
+colcon build
+source install/local_setup.bash
+ros2 run rover_control diff_test 2.3 1.5 2.0 2.0
+# args: [right, left, forward, backward]
 ```
--- ros2_ws/src/rover_control/rover_control/diff_drive_test.py
+++ ros2_ws/src/rover_control/rover_control/diff_drive_test.py
@@ -3,10 +3,11 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 import math
+import sys
 
 class RoverController(Node):
 
-    def __init__(self):
+    def __init__(self, state_times):
         super().__init__('rover_controller')
 
         self.left_wheel_publisher = self.create_publisher(Twist, '/diff_drive_controller_left/cmd_vel_unstamped', 10)
@@ -36,9 +37,11 @@ class RoverController(Node):
         self.right_linear_velocity = 0.0
 
         self.state_start_time = self.get_clock().now().nanoseconds / 1e9
-        self.state_times = [2.3, 3.0, 0.0]  # Czas jazdy w lewo, w prawo, oraz zatrzymanie
+        self.state_times = state_times  # Użycie przekazanych argumentów
         self.state_end_time = [self.state_start_time + self.state_times[0], 
                                self.state_start_time + self.state_times[0] + self.state_times[1], 
+                               self.state_start_time + self.state_times[0] + self.state_times[1] + self.state_times[2], 
+                               self.state_start_time + self.state_times[0] + self.state_times[1] + self.state_times[2] + self.state_times[3], 
                                0.0]  # Czas zakończenia dla każdego etapu
 
         self.timer_period = 0.1  # Interwał aktualizacji
@@ -66,6 +69,14 @@ class RoverController(Node):
             left_twist.linear.x = -2.0  # Ruch w prawo (lewą stroną do tyłu)
             right_twist.linear.x = 2.0  # Ruch w prawo (prawą stroną do przodu)
             self.get_logger().info("State 1: Moving right")
+        elif current_time < self.state_end_time[2]:  # Jazda w prawo przez 2 sekundy
+            left_twist.linear.x = 2.0  # Ruch w prawo (lewą stroną do tyłu)
+            right_twist.linear.x = 2.0  # Ruch w prawo (prawą stroną do przodu)
+            self.get_logger().info("State 1: Moving right")
+        elif current_time < self.state_end_time[3]:  # Jazda w prawo przez 2 sekundy
+            left_twist.linear.x = -2.0  # Ruch w prawo (lewą stroną do tyłu)
+            right_twist.linear.x = -2.0  # Ruch w prawo (prawą stroną do przodu)
+            self.get_logger().info("State 1: Moving right")
         else:  # Zatrzymanie
             left_twist.linear.x = 0.0
             right_twist.linear.x = 0.0
@@ -89,20 +100,34 @@ class RoverController(Node):
         self.get_logger().info(f"Position: x = {self.x:.2f}, y = {self.y:.2f}, theta = {self.theta:.2f}")
 
         # Po zakończeniu jazdy, zatrzymanie i anulowanie timera
-        if current_time >= self.state_end_time[1]:
-            self.state_end_time[2] = current_time
+        if current_time >= self.state_end_time[3]:
+            self.state_end_time[4] = current_time
             self.timer.cancel()  # Zatrzymujemy timer, bo cała sekwencja się zakończyła
 
 def main(args=None):
     rclpy.init(args=args)
 
-    rover_controller = RoverController()
+    # Przetwarzanie argumentów z wiersza poleceń
+    if len(sys.argv) != 5:
+        print("Usage: ros2 run rover_control diff_test <time_left> <time_right>")
+        sys.exit(1)
+    
+    try:
+        time_left = float(sys.argv[1])
+        time_right = float(sys.argv[2])
+        time_forward = float(sys.argv[3])
+        time_backward = float(sys.argv[4])
+    except ValueError:
+        print("Error: Time values must be numeric.")
+        sys.exit(1)
+
+    # Tworzenie instancji kontrolera z przekazanymi czasami
+    rover_controller = RoverController(state_times=[time_left, time_right, time_forward, time_backward, 0.0])
 
     rclpy.spin(rover_controller)
 
     rover_controller.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
